15b50a5995a1c15e988734107f49565571aa801f
Hatim Ghadiyali <hatimg@gmail.com>
2024-10-23 12:39:27 +0100

    Add repository for Book

---
+++ src/main/java/com/assignment/library/repository/BookRepository.java
@@ -0,0 +1,14 @@
+package com.assignment.library.repository;
+
+import com.assignment.library.model.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, String> {
+
+    List<Book> findByAuthor(String author);
+
+}
