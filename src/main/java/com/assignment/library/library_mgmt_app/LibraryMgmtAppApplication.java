223bf97b82e0816684caef8a7abca86d3689c04a
Hatim Ghadiyali <hatimg@gmail.com>
2024-10-23 12:19:01 +0100

    Setup maven project and dependencies

---
+++ src/main/java/com/assignment/library/library_mgmt_app/LibraryMgmtAppApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.library.library_mgmt_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryMgmtAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryMgmtAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/library/library_mgmt_app/LibraryMgmtAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.library.library_mgmt_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryMgmtAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
